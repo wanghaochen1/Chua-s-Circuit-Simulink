ee9cddea6a3e67c82560b356496edd3bbb3f8f6b
wanghaochen1 <wanghaochen001@outlook.com>
2024-04-08 23:53:27 +0800

    New graph
    多个吸引子

--- Differernt_Start.m
+++ Differernt_Start.m
@@ -1,4 +1,4 @@
-clc,clear
+clc, clear
 legendInfo = {}; % Initialize cell array to hold legend info
 frames = [];
 %%
@@ -12,40 +12,50 @@ el = 10; % Initialize elevation
 figure
 grid on
 hold on
+
 for a = 1:0.1:1.2
+
     for b = 0.5:0.1:0.6
+
         for c = 0.2:0.1:0.3
-            y0 = [a;b;c];
+            y0 = [a; b; c];
             [t, y] = ode45(@myODE, tspan, y0);
-            plot3(y(:,1),y(:,2),y(:,3));
+            plot3(y(:, 1), y(:, 2), y(:, 3));
             legendInfo{idx} = ['Initial conditions: ', num2str(a), ', ', num2str(b), ', ', num2str(c)];
             idx = idx + 1;
             view(az, el) % Change view
             hold on
+
             if MOVIE
                 frame = getframe; % Capture the plot as a frame
                 frames = [frames frame];
                 drawnow
                 im = frame2im(frame);
-                [imind,cm] = rgb2ind(im,256);
+                [imind, cm] = rgb2ind(im, 256);
+
                 if a == 1 && b == 0.5 && c == 0.2
-                    imwrite(imind,cm,filename,'gif', 'Loopcount',inf); % Write to the GIF file
+                    imwrite(imind, cm, filename, 'gif', 'Loopcount', inf); % Write to the GIF file
                 else
-                    imwrite(imind,cm,filename,'gif','WriteMode','append'); % Append to the GIF file
+                    imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append'); % Append to the GIF file
                 end
+
             else
                 hold on
             end
+
             % az = az + 1; % Increment azimuth
             % el = el + 1; % Increment elevation
         end
+
     end
+
 end
+
 legend(legendInfo)
+
 if MOVIE
     movie(frames)
 end
-
 
 % y0 = [1; 0.5; 0]; % initial conditions
 % [t, y] = ode45(@myODE, tspan, y0);
@@ -58,24 +68,24 @@ end
 %% Chua's Parameters
 function dydt = myODE(t, y)
 
-G = 0.7;
-C1 = 1/9;
-C2 = 1;
-L=1/7;
-R=0.005;
+    G = 0.7;
+    C1 = 1/9;
+    C2 = 1;
+    L = 1/7;
+    R = 0.005;
 
-v1 = y(1);
-v2 = y(2);
-i3 = y(3);
-dydt = zeros(3,1);
-dydt(1) = 1/C1 * (G*(v2 - v1)-f(v1));
-dydt(2) = 1/C2 * (G*(v1 - v2) + i3);
-dydt(3) = -1/L *(v2+R*i3);
+    v1 = y(1);
+    v2 = y(2);
+    i3 = y(3);
+    dydt = zeros(3, 1);
+    dydt(1) = 1 / C1 * (G * (v2 - v1) - f(v1));
+    dydt(2) = 1 / C2 * (G * (v1 - v2) + i3);
+    dydt(3) = -1 / L * (v2 + R * i3);
 end
 
 function y = f(v1)
-Ga=-0.8;
-Gb=-0.5;
-E = 1;
-y = Gb*v1 + 0.5*(Ga - Gb)*(abs(v1+E)-abs(v1-E));
+    Ga = -0.8;
+    Gb = -0.5;
+    E = 1;
+    y = Gb * v1 + 0.5 * (Ga - Gb) * (abs(v1 + E) - abs(v1 - E));
 end
---
+++ multiple.m
@@ -0,0 +1,55 @@
+% This progarme genertes Multiple attracters in Chua's circuit
+% using triangular wave
+% Author: 王昊宸
+% 这个代码没有使用传统的电路建模方式，而是直接基于微分方程进行优化
+clc, clear
+tspan = [0 250];
+filename = 'result/Multiple_attractor.gif';
+%%
+y0 = [0.5; 0.5; 0.5]; % initial conditions
+[t, y] = ode45(@myODE, tspan, y0);
+plot3(y(:, 1), y(:, 2), y(:, 3));
+grid on
+for j = 1:10:length(t)
+    % Draw plot for y upto current timepoint
+    plot3(y(1:j, 1), y(1:j, 2), y(1:j, 3));
+    view(mod(j / 10, 360), 5);
+    grid on
+    drawnow
+    % Capture the plot as an image
+    frame = getframe(gcf);
+    im = frame2im(frame);
+    [imind, cm] = rgb2ind(im, 256);
+    % Change view angle
+    % Write to the GIF File
+    if j == 1
+        imwrite(imind, cm, filename, 'gif', 'Loopcount', inf);
+    else
+        imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append');
+    end
+
+end
+%%
+function dydt = myODE(t, y)
+    N = 5;
+    alpha = 4.2;
+    beta = 6.7;
+    gamma = 4.0;
+    epson = 11.6;
+    dydt = zeros(3, 1);
+    X = y(1);
+    Y = y(2);
+    Z = y(3);
+    dydt(1) = beta * Y - X - alpha * f(X, N);
+    dydt(2) = beta * X - gamma * Z;
+    dydt(3) = epson * Y - Z;
+end
+
+function sum = f(x, N)
+    sum = x;
+
+    for n = 1:2 * N - 1
+        sum = sum + (-1) ^ (n) * (abs(x + (2 * n - 1)) - abs(x - (2 * n - 1)));
+    end
+
+end
