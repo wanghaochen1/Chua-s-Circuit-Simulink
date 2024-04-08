26b4c5f2f9bc08413e62db5d6de5964a8468ca95
wanghaochen1 <wanghaochen001@outlook.com>
2024-04-09 00:23:39 +0800

    基于一个奇怪的分段函数的吸引子
    我觉得这个理论有问题

---
+++ Ultra.m
@@ -0,0 +1,98 @@
+% This progarme genertes Multiple attracters in Chua's circuit
+% using triangular wave
+% Author: 王昊宸
+% 这个代码没有使用传统的电路建模方式，而是直接基于微分方程进行优化
+clc, clear
+tspan = [0 250];
+filename = 'result/Multiple_attractor.gif';
+%%
+y0 = [0.5; 0.5; 0.5]; % initial conditions
+[t, y] = ode45(@myODE, tspan, y0);
+plot3(y(:, 1), y(:, 2), y(:, 3));
+grid on
+
+% for j = 1:10:length(t)
+%     % Draw plot for y upto current timepoint
+%     plot3(y(1:j, 1), y(1:j, 2), y(1:j, 3));
+%     view(mod(j / 10, 360), 5);
+%     grid on
+%     drawnow
+%     % Capture the plot as an image
+%     frame = getframe(gcf);
+%     im = frame2im(frame);
+%     [imind, cm] = rgb2ind(im, 256);
+%     % Change view angle
+%     % Write to the GIF File
+%     if j == 1
+%         imwrite(imind, cm, filename, 'gif', 'Loopcount', inf);
+%     else
+%         imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append');
+%     end
+%
+% end
+
+%%
+function dydt = myODE(t, y)
+    N = 6;
+    M = 6;
+    alpha = 4.2;
+    beta = 6.7;
+    gamma = 4.0;
+    epson = 11.8;
+    dydt = zeros(3, 1);
+    X = y(1);
+    Y = y(2);
+    Z = y(3);
+    dydt(1) = beta * Y - X - alpha * f(X, N, M);
+    dydt(2) = beta * X - gamma * Z;
+    dydt(3) = epson * Y - Z;
+end
+
+function sum = f(x, N, M)
+    sum = x - h0(x);
+
+    for n = 1:N
+        sum = sum - h_plus(x - 2 * n);
+    end
+
+    for m = 1:M
+        sum = sum - h_minus(x + 2 * m);
+    end
+
+end
+
+function y = h0(x)
+
+    if x <- 1
+        y = -1;
+    elseif x > 1
+        y = 1;
+    else
+        y = x;
+    end
+
+end
+
+function y = h_minus(x)
+
+    if x <- 1
+        y = -2;
+    elseif x > 1
+        y = 0;
+    else
+        y = x;
+    end
+
+end
+
+function y = h_plus(x)
+
+    if x > 1
+        y = 2;
+    elseif x < -1
+        y = 0;
+    else
+        y = x;
+    end
+
+end
